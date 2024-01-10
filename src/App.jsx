3ff1eb230f34ae3e9a8a0e1c26d7a5d3e2c79c62
Gustavo Miller <gustavo.miller@miller-hs.com>
2024-01-10 08:11:04 -0500

    Insert developing header into code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+/*******************************************************************
+ * Carleton Bootcamp - 2024
+ * Copyright 2024 Gustavo Miller
+ * License: free and unencumbered software
+ * Assignment # 20 - React Portfolio
+ * 
+ * Date : 1/10/2024 8:03:28 AM
+ *******************************************************************/
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+/*******************************************************************
+ * Carleton Bootcamp - 2024
+ * Copyright 2024 Gustavo Miller
+ * License: free and unencumbered software
+ * Assignment # 20 - React Portfolio
+ * 
+ * Date : 1/10/2024 8:03:28 AM
+ *******************************************************************/
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
